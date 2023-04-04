04263aadf38576f55085cf828e55695f418e535d
TearsOprah <qmeshokp@mail.ru>
2023-04-04 18:13:21 +0300

    feat: болванка для массива с проектами

---
+++ src/utils/projects.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    'title': 'Название первого проекта',
+    'image': '',
+    'description': '',
+    'demoLink': '',
+    'codeLink': '',
+    'stack': ['JS', 'React', 'БЭМ', ]
+  },
+  {
+    'title': 'Название второго проекта',
+    'image': '',
+    'description': '',
+    'demoLink': '',
+    'codeLink': '',
+    'stack': ['JS', 'React', 'БЭМ', ]
+  }
+]
