a97d7de52e05cc656cfc8896f9f1201da9ec4f3a
TearsOprah <qmeshokp@mail.ru>
2023-04-04 17:09:30 +0300

    feat: компоненты для страниц с проектами и ничего не найдено, прописал роуты

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import {Route, Routes} from "react-router-dom";
+import Projects from "./components/Projects/Projects";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/projects'} element={<Projects />} />
+        <Route path={'*'} element={<NotFound />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+  return (
+    <h2>Все проекты</h2>
+  )
+}
