9f67a76d8d64555199ed994c8a0d15558cd61fe1
TearsOprah <qmeshokp@mail.ru>
2023-04-21 13:01:07 +0300

    feat: верстка header, накидывание класса для активной ссылки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
-import {useState, useEffect} from "react";
-import './Header.scss'
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import texts from "../../utils/texts.js";
+import "./Header.scss";
 
-export default function Header({setCurrentLang, currentLang}) {
-
+export default function Header({ setCurrentLang, currentLang }) {
   const navLinks = [
-    { title: texts[currentLang].home, path: "/" },
-    { title: texts[currentLang].projects, path: "/projects" },
-    { title: texts[currentLang].contacts, path: "/contacts" },
+    { title: texts[currentLang].home, path: "/", color: "red" },
+    { title: texts[currentLang].projects, path: "/projects", color: "green" },
+    { title: texts[currentLang].contacts, path: "/contacts", color: "blue" },
   ];
 
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [activeLink, setActiveLink] = useState(window.location.pathname);
 
   const handleMenuClick = () => {
     setIsOpenMenu(!isOpenMenu);
@@ -27,37 +27,51 @@ export default function Header({setCurrentLang, currentLang}) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // смена языка
   const handleLang = () => {
-    currentLang === 'ru' ? setCurrentLang('en') : setCurrentLang('ru')
-  }
+    currentLang === "ru" ? setCurrentLang("en") : setCurrentLang("ru");
+  };
 
   return (
     <header className="header">
-
-      <div className={'header__branding'}>
-        <h2>TearsOprah</h2>
-        <div>Logo</div>
+      <div className={"header__branding"}>
+        <h2 className={"header__title"}>TearsOprah</h2>
+        <div className={"header__logo"}>Logo</div>
       </div>
 
       <nav className="nav">
+        <button className="menu-icon" onClick={handleMenuClick}>
+          Меню
+        </button>
 
-          <button className="menu-icon" onClick={handleMenuClick}>
-            Меню
+        <ul className={`nav__list ${isOpenMenu ? "nav__list--open" : ""}`}>
+          {navLinks.map((link, index) => (
+            <li
+              className={`nav__item ${
+                activeLink === link.path ? "nav__item-active" : ""
+              }`}
+              key={index}
+            >
+              <div
+                className="nav__icon"
+                style={{ backgroundColor: link.color }}
+              ></div>
+              <Link
+                to={link.path}
+                className={`nav__link`}
+                onClick={() => setActiveLink(link.path)}
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
+          <button
+            className={"header__button"}
+            type="button"
+            onClick={handleLang}
+          >
+            {currentLang === "ru" ? "en" : "ru"}
           </button>
-
-          <ul className={`nav__list ${isOpenMenu ? "nav__list--open" : ""}`}>
-            {navLinks.map((link, index) => (
-              <li className="nav__item" key={index}>
-                <Link to={link.path} className="nav__link">
-                  {link.title}
-                </Link>
-              </li>
-            ))}
-            <button type="button">тема</button>
-            <button type="button" onClick={handleLang}>{currentLang}</button>
-          </ul>
-
+        </ul>
       </nav>
     </header>
   );
