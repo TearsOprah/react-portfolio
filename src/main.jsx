639263d0ccc7b548857d0d32edf5d99fc2c4a826
TearsOprah <qmeshokp@mail.ru>
2023-04-29 13:12:53 +0300

    feat: метрика настройка

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import YandexMetrika from "./components/YandexMetrika.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <YandexMetrika />
     <App />
   </BrowserRouter>,
 )
