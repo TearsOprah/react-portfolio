52eca4d5bd61613ab51e49ebce45f005cb0e948a
TearsOprah <qmeshokp@mail.ru>
2023-06-26 15:04:09 +0300

    feat: добавлено новое тестовое задание

--- src/utils/testTasks.js
+++ src/utils/testTasks.js
@@ -1,5 +1,6 @@
 import test_1_scr from "../assets/test_1_scr.png";
 import test_1 from "../assets/test_1.png";
+import test_2 from "../assets/test_2.png";
 
 const testTasks = [
   {
@@ -11,6 +12,15 @@ const testTasks = [
     'codeLink': 'https://github.com/TearsOprah/frontend-test-aaccent',
     'stack': ['JS', 'React', 'Adaptive', 'Api' ],
     'id': 0,
+  },
+  {
+    'title': {'ru': 'Корпоративный многостраничный сайт', 'en': 'Corporate website'},
+    'image': test_2,
+    'description': {'ru': 'В задании было предложено написать корпоративный сайт. К заданию был приложен дизайн главной страницы в формате psd для desktop экрана. В итоге мною был сделан адаптивный многостраничный сайт в виде spa приложения на React', 'en': 'In the assignment, it was suggested to write a corporate website. The design of the main page in psd format for the desktop screen was attached to the task. As a result, I made an adaptive multi-page website in the form of a spa application on React'},
+    'demoLink': 'https://nls-test.vercel.app/',
+    'codeLink': 'https://github.com/TearsOprah/NLS-test',
+    'stack': ['JS', 'React', 'Adaptive'],
+    'id': 1,
   },
 ]
 
